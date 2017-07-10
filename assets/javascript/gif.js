f11d58209d8ff6abc610da1b4d701b8167bd9283
Helmut Cardenas <hcardenas@Helmuts-MBP.attlocal.net>
2017-07-09 20:34:15 -0400

    made the basic layout

---
+++ assets/javascript/gif.js
@@ -0,0 +1,9 @@
+console.log("im rdy");
+
+var sports_arr = ["crossfit", "speedwalking", "soccer", "basketball", "softball", "baseball", "pool", "poker", 
+	"running", "jogging", "skating", "rollerskating", "swiming", "bowling", "waterski"];
+
+
+$(document).ready( function() {
+	
+});
