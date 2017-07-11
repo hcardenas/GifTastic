0b6aa3304d77e3e533812db3c03e0f681324378a
Helmut Cardenas <hcardenas@Helmuts-MBP.attlocal.net>
2017-07-11 11:31:35 -0400

    fixed wrong input and layout of images

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -30,7 +30,23 @@ $("#button_div").on("click", ".button_style", function() {
     });
 });
 
+$("#images_div").on("click", ".img", function(){
+	console.log(this);
+	console.log("i was clicked: " + this.alt);
+	var index = parseInt(this.alt)
+	if (images_arr[index].flag) 
+		this.src = images_arr[index].gif_url;
+	else 
+		this.src = images_arr[index].still_img;
+	
+
+	images_arr[index].flag = !images_arr[index].flag;
+
+});
+
+
 function display_images(json_response) {
+	console.log(json_response);
 	var gif_url;
 	var rating;
 	var still_img;
@@ -38,14 +54,15 @@ function display_images(json_response) {
 	var width;
 	var image_obj = {};
 
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < json_response.data.length; i++) {
 		
-		image_obj.gif_url = json_response.data[i].embed_url;
+		image_obj.gif_url = json_response.data[i].images.fixed_height.url;
 		image_obj.rating = json_response.data[i].rating;
-		image_obj.still_img = json_response.data[i].images.fixed_width_still.url;
-		image_obj.height = json_response.data[i].images.fixed_width_still.height;
-		image_obj.width = json_response.data[i].images.fixed_width_still.width;
-		console.log(image_obj);
+		image_obj.still_img = json_response.data[i].images.fixed_height_still.url;
+		image_obj.height = json_response.data[i].images.fixed_height_still.height;
+		image_obj.width = json_response.data[i].images.fixed_height_still.width;
+		image_obj.flag = true;
+		
 
 		// push to imaes array to use later when clicling on images 
 		images_arr.push(image_obj);
@@ -59,18 +76,32 @@ function display_images(json_response) {
 
 	for (var i = 0; i < images_arr.length; i++) {
 		
-		outter_div = $("<div class='image_class'>");
-		rating_div = $("<p>").text(images_arr[i].rating);
-		image_div = $('<img src=' + images_arr[i].still_img + ' style="width:' +images_arr[i].width +'px;height:'+images_arr[i].height+'px;">');
+		outter_div = $("<div class='image_class' style='float:left;' >  " );
+		rating_div = $("<p>").text("Rating: " + images_arr[i].rating);
+		image_div = $('<img alt="'+i+'" class="img-thumbnail img" src=' + images_arr[i].still_img + ' style="width:' + images_arr[i].width + 'px;height:' + images_arr[i].height +'px;" >');
 
 		outter_div.append(rating_div);
 		outter_div.append(image_div);
 
 		$("#images_div").append(outter_div);
-
 	}
 		
 }
+
+function valid_req (req) {
+	
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q= " + req + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	$.ajax({
+    	url: queryURL,
+    	method: 'GET'
+    }).done(function(response) {
+    	console.log("function returned: " + response.meta.status === 200);
+    	return (response.meta.status === 200) 
+
+    });
+}
+
 function create_button (button_name) {
 	var new_button = $('<button type="button" class="btn btn-success btn-md button_style" value=' + button_name + '>').text(button_name.replace(/[+]+/g, " "));
 	$("#button_div").append(new_button);
@@ -81,11 +112,11 @@ function add_button_from_input() {
 	var input_from_user = $.trim($("#input_div #add_a_button").val() );
 	input_from_user = input_from_user.replace(/[ ]+/g, "+").toLowerCase();
 
-	if (sports_arr.indexOf(input_from_user) === -1) {
+	if (sports_arr.indexOf(input_from_user) === -1 && valid_req(input_from_user) ) {
 		sports_arr.push(input_from_user);
 		create_button(input_from_user);
 		console.log(input_from_user + " added to array and screen");
 	} else {
-		console.log("it Already exits in the array");
+		console.log("it Already exits in the array or not valid input");
 	}
 }
